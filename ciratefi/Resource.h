748b626731572dc51c666ded188c7104f8befbdc
Yao-Bin Yang <andy79923@gmail.com>
2014-03-07 10:06:32 +0800

    Add Picture control to show the source image be loaded The OpenCV class Mat can not be directly showed on the MFC Picture control, it must translate to another type that Picture Control can show directly.

--- ciratefi/Resource.h
+++ ciratefi/Resource.h
@@ -8,6 +8,7 @@
 #define IDD_CIRATEFI_DIALOG             102
 #define IDR_MAINFRAME                   128
 #define IDC_BUTTONLOADSOURCE            1000
+#define IDC_PICTURESOURCEIMAGE          1001
 
 // Next default values for new objects
 // 
@@ -15,7 +16,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
